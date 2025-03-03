e761327a1a78d19b0ddf7e13d2940438bd3999e8
gomadango0113 <gomadango0113@gmail.com>
2025-03-03 20:20:50 +0900

    【追加】基本構造を追加

--- src/main/java/org/gomadango0113/blockwars/BlockWars.java
+++ src/main/java/org/gomadango0113/blockwars/BlockWars.java
@@ -1,17 +1,34 @@
 package org.gomadango0113.blockwars;
 
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BlockWars extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        saveDefaultConfig();
+        registerCommand();
+        registerListener();
 
+        getLogger().info("[BlockWars] プラグインの準備ができました。");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
+
+    public void registerCommand() {
+
+    }
+
+    public void registerListener() {
+        PluginManager plm = getServer().getPluginManager();
+    }
+
+    public static BlockWars getInstance() {
+        return getPlugin(BlockWars.class);
+    }
+
 }
