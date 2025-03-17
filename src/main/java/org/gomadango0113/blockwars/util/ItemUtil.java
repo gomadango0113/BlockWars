fd5ee9d576771f2dac96e5828825a41ee0c2ac1d
gomadango0113 <gomadango0113@gmail.com>
2025-03-17 20:04:00 +0900

    【追加】ItemUtilを追加

---
+++ src/main/java/org/gomadango0113/blockwars/util/ItemUtil.java
@@ -0,0 +1,38 @@
+package org.gomadango0113.blockwars.util;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemUtil {
+
+    private ItemStack itemstack;
+
+    public ItemUtil(Material material) {
+        this.itemstack = new ItemStack(material);
+    }
+
+    public ItemStack getItemStack(String name, List<String> lore) {
+        ItemMeta meta = itemstack.getItemMeta();
+        List<String> get_lore = meta.getLore() == null ? new ArrayList<>() : new ArrayList<>(meta.getLore());
+        if (lore != null) {
+            get_lore.addAll(lore);
+        }
+        setNameLore(itemstack, name, get_lore);
+
+        return itemstack;
+    }
+
+    public static ItemStack setNameLore(ItemStack itemStack, String name, List<String> lore) {
+        ItemMeta meta = itemStack.getItemMeta();
+        if (name != null) meta.setDisplayName(name);
+        if (lore != null) meta.setLore(lore);
+        itemStack.setItemMeta(meta);
+
+        return itemStack;
+    }
+
+}
